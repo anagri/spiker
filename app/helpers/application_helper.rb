d445e2febeccca184e0ee712a1241dcba0b85964
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-09 12:26:42 +0530

    [Amir] have doctype helper and using same in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def doctype
+    %q{<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">}
+  end
+
+  def html
+    %q{<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">}
+  end
+
+  def end_html
+    %q{</html>}
+  end
 end
