adba6ea689992aadb8230cf4697eea87c3bb2c23
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-12 00:50:14 +0530

    [Amir] HerokuTask : removed vendor/gems, slug to deploy on heroku was taking lot of time

--- vendor/gems/remarkable-3.1.13/lib/remarkable/version.rb
+++
@@ -1,3 +0,0 @@
-module Remarkable
-  VERSION = '3.1.13' unless self.const_defined?(:VERSION)
-end
--- vendor/gems/webrat-rspec-rails-0.1.1/lib/webrat_rspec_rails/extensions/webrat_methods.rb
+++
@@ -1,5 +0,0 @@
-module Webrat
-  module Methods
-    delegate_to_session :current_scope, :field_named, :key_up, :fire_event
-  end
-end
