ab1c9b4bcd0defca00ebc6ed3b00215991d66131
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-12 21:32:08 +0530

    [Amir] mind map file for various accouting packages review

---
+++ doc/AccoutingPackagesReview.mm
@@ -0,0 +1,98 @@
+<map version="0.7.1">
+<node TEXT="Accouting Packages">
+<node TEXT="Freshbooks" POSITION="right">
+<node TEXT="User Registration"/>
+<node TEXT="User Login"/>
+<node TEXT="Authenticated Features">
+<node TEXT="Accouting Related">
+<node TEXT="Create Client"/>
+<node TEXT="Create Invoice">
+<node TEXT="Type">
+<node TEXT="Time"/>
+<node TEXT="Quantity"/>
+</node>
+<node TEXT="Invoice">
+<node TEXT="Choose Customer">
+<node TEXT="New Customer">
+<node TEXT="Create a customer partial as popup"/>
+</node>
+<node TEXT="Existing Customer"/>
+</node>
+<node TEXT="Currency">
+<node TEXT="Very efficient screen space usage"/>
+</node>
+<node TEXT="Language"/>
+<node TEXT="Invoice #"/>
+<node TEXT="Date"/>
+<node TEXT="PO #"/>
+<node TEXT="Discount %"/>
+<node TEXT="Invoice Table">
+<node TEXT="Item drop down search/select"/>
+<node TEXT="Item name">
+<node TEXT="Autofill"/>
+</node>
+<node TEXT="Item description">
+<node TEXT="Autofill"/>
+</node>
+<node TEXT="Unit Cost"/>
+<node TEXT="Qty"/>
+<node TEXT="Tax Type">
+<node TEXT="Edit/Add tax with a popup"/>
+</node>
+<node TEXT="Tax"/>
+<node TEXT="Line Total"/>
+</node>
+<node TEXT="Invoice Grand Total">
+<node TEXT="Tax calculation"/>
+</node>
+</node>
+</node>
+<node TEXT="Create Project"/>
+</node>
+<node TEXT="Utils">
+<node TEXT="Upload logo"/>
+<node TEXT="Add members"/>
+<node TEXT="Customize mails"/>
+</node>
+<node TEXT="Tabs">
+<node TEXT="Home"/>
+<node TEXT="Clients"/>
+<node TEXT="Team"/>
+<node TEXT="Invoices"/>
+<node TEXT="Estimates"/>
+<node TEXT="Expenses"/>
+<node TEXT="Time Tracking"/>
+<node TEXT="Reports"/>
+<node TEXT="...manage tabs">
+<node TEXT="authorization">
+<node TEXT="Very basic">
+<node TEXT="Roles">
+<node TEXT="Adminstrator"/>
+<node TEXT="Staff"/>
+<node TEXT="Clients"/>
+</node>
+</node>
+<node TEXT="Tabs based feature access"/>
+</node>
+</node>
+</node>
+<node TEXT="Dashboard">
+<node TEXT="Invoices Summary"/>
+<node TEXT="Feature/Config advert"/>
+</node>
+</node>
+</node>
+<node TEXT="GNUCash" POSITION="left">
+<node TEXT="Types">
+<node TEXT="File"/>
+<node TEXT="Account"/>
+<node TEXT="Transaction"/>
+</node>
+<node TEXT="New">
+<node TEXT="Duid">
+<node TEXT="Account Creation through Grouping"/>
+</node>
+</node>
+</node>
+</node>
+</map>
