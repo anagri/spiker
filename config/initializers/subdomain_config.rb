c19a053366020c063d8be7141ef43edfce9b2631
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-18 09:52:00 +0530

    [Amir] adding and commenting subdomain feature

---
+++ config/initializers/subdomain_config.rb
@@ -0,0 +1,11 @@
+#SubdomainFu.tld_size = 1 # sets for current environment
+#SubdomainFu.tld_sizes = {:development => 0,
+#                         :test => 0,
+#                         :production => 1} # set all at once (also the defaults)
+#
+#SubdomainFu.mirrors = %w(www) # Defaults to %w(www)
+#
+#SubdomainFu.preferred_mirror = "www"
+
+#  config.gem "subdomain-fu", :version => '0.5.4'
+
