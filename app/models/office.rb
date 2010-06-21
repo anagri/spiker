2de47fc300048dc13dd7d079cb46e73621dc3364
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-21 23:22:27 +0530

    [Amir] office active record model

---
+++ app/models/office.rb
@@ -0,0 +1,7 @@
+class Office < ActiveRecord::Base
+  enhanced_acts_as_tree
+  alias_method :head_office?, :root?
+
+  validates_uniqueness_of :name
+  validates_presence_of :parent, :if => Proc.new {Office.root?}
+end
