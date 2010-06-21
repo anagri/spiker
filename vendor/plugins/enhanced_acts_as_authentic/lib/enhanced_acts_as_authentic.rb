5198cb7371ee9b5e549f0f6cf91d4e086dab3e63
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-21 10:42:44 +0530

    [Amir] extending enhanced acts as authentic to authlogic session

--- vendor/plugins/enhanced_acts_as_authentic/lib/enhanced_acts_as_authentic.rb
+++ vendor/plugins/enhanced_acts_as_authentic/lib/enhanced_acts_as_authentic.rb
@@ -2,20 +2,16 @@ module EnhancedActsAsAuthentic
   def self.included(klass)
     klass.class_eval do
       extend ClassMethods
-      extend Authorization::Maintenance
       include InstanceMethods
     end
   end
 
-  module ClassMethods
-    def enhanced_acts_as_authentic
-      acts_as_authentic
-    end
-
+  module MethodMissing
+    include Authorization::Maintenance
     def method_missing(symbol, *args)
       if symbol.to_s =~ /^without_access_control_do_(.*)$/
         resource = without_access_control do
-          self.send $1.to_sym, args.shift, *args
+          self.send $1.to_sym, *args
         end
         return resource
       end
@@ -23,8 +19,18 @@ module EnhancedActsAsAuthentic
     end
   end
 
-  module InstanceMethods
+  module ClassMethods
+    include EnhancedActsAsAuthentic::MethodMissing
+    def enhanced_acts_as_authentic
+      acts_as_authentic
+    end
   end
+
+  module InstanceMethods
+    include EnhancedActsAsAuthentic::MethodMissing
+  end
+
 end
 
 ActiveRecord::Base.send :include, EnhancedActsAsAuthentic
+Authlogic::Session::Base.send :include, EnhancedActsAsAuthentic
