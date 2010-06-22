a29f4c23ed4ef4745dc80de9bd0c393af6b9b72b
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-22 16:22:51 +0530

    [Amir] (broken commit) added limit check in database and model

--- app/models/office_type.rb
+++ app/models/office_type.rb
@@ -1,7 +1,9 @@
 class OfficeType < ActiveRecord::Base
   using_access_control
-  
+
   validates_uniqueness_of :name
+  validates_length_of :first_name, :maximum=>30
+
   validates_presence_of :parent, :if => Proc.new {OfficeType.root?}
 
   belongs_to :parent, :class_name => OfficeType
@@ -9,6 +11,7 @@ class OfficeType < ActiveRecord::Base
   has_one :child, :class_name => OfficeType, :foreign_key => :parent_id
 
   has_many :offices
+
   def self.root
     find(:first, :conditions => "PARENT_ID IS NULL")
   end
--- db/migrate/20100622084757_create_office_types_table.rb
+++ db/migrate/20100622084757_create_office_types_table.rb
@@ -1,7 +1,7 @@
 class CreateOfficeTypesTable < ActiveRecord::Migration
   def self.up
     create_table :office_types do |t|
-      t.string :name, :null => false
+      t.string :name, :limit => 30, :null => false
       t.references :parent
       t.timestamps
     end
