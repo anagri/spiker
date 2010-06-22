b71decfaee2307b62bd0bd21d8b8bae4eb9d6837
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-22 17:26:26 +0530

    [Amir] removed enhanced enum attribute and enum attribute

--- vendor/plugins/enhanced_enum_attr/init.rb
+++
@@ -1,3 +0,0 @@
-gem 'enumerated_attribute', '>= 0.2.13'
-
-require File.expand_path(File.dirname(__FILE__) + '/lib/enhanced_enum_attr')
--- vendor/plugins/enhanced_enum_attr/lib/tasks/enhanced_enum_attr.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :enhanced_enum_attr do
-#   # Task goes here
-# end
