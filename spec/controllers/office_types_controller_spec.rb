c995e190ac95c83f3ba5558f49c2a4b774c0f76e
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-26 20:13:58 +0530

    [Amir] added specs for office types authorization

--- spec/controllers/office_types_controller_spec.rb
+++ spec/controllers/office_types_controller_spec.rb
@@ -3,11 +3,10 @@ require File.dirname(__FILE__) + '/spec_helper'
 describe OfficeTypesController do
   describe 'authorization' do
     before(:each) do
+      @create_params = {:office_type => {:name => 'test office type'}}
       @resource_id = Factory.without_access_control_do_create(:office_type).id
-      @user = admin
-      @create_params = {:office_type => {:name => 'office type 1'}}
     end
 
-    it_should_behave_like 'authorized controller'
+    it_should_behave_like 'authenticated user staff viewed and admin managed controller'
   end
 end
