985495765c56a22eccfaed737d5a1421dae6fe15
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-26 20:22:18 +0530

    [Amir] refactored offices controller to new shared spec

--- spec/controllers/offices_controller_spec.rb
+++ spec/controllers/offices_controller_spec.rb
@@ -1,36 +1,12 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe OfficesController do
-  before(:each) do
-    @resource_id = Factory.without_access_control_do_create(:office).id
-    @create_params = {:office => {:name => 'office name', :parent => Office.root, :office_type => Factory.without_access_control_do_create(:office_type)}}
-  end
-
   describe 'authorization' do
-    describe 'for admin' do
-      before(:each) do
-        @user = admin
-      end
-
-      it_should_behave_like 'authorized controller'
+    before(:each) do
+      @resource_id = Factory.without_access_control_do_create(:office).id
+      @create_params = {:office => {:name => 'office name', :parent_id => Office.root.id, :office_type_id => Factory.without_access_control_do_create(:office_type).id}}
     end
 
-    describe 'for staff' do
-      before(:each) do
-        @user = staff
-        @allowed_actions = view_actions
-      end
-
-      it_should_behave_like 'authorized controller'
-    end
-
-    describe 'for guest' do
-      before(:each) do
-        @user = guest
-        @allowed_actions = none_actions
-      end
-
-      it_should_behave_like 'authorized controller'
-    end
+    it_should_behave_like 'authenticated user staff viewed and admin managed controller'
   end
 end
