dedf321fc086d972fdaea8afc1c1109f263cdd3e
Amiruddin Nagri <amir.nagri@gmail.com>
2010-06-28 19:31:27 +0530

    [Amir] removed prototype, introduced jquery, having focus on first element with class focus

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+/*
+focus the element with class focus
+ */
+$(function() {
+    $('.focus').first().focus();
+});
