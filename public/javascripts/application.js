9fd3cef884d3b2b117818a97eb41e4d2e510a116
Amiruddin Nagri <amir.nagri@gmail.com>
2010-07-11 03:15:09 +0530

    [Amir] made all the javascript into jquery widgets

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,82 +5,13 @@ window.onerror = function(msg, file, line) {
     console.log(msg + ',' + file + ',' + line);
 };
 
-$.extend({
-    urlParams: function() {
-        var vars = [], hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).replace(/#.*$/gi, '').split('&');
-        for (var i = 0; i < hashes.length; i++) {
-            hash = hashes[i].split('=');
-            vars.push(hash[0]);
-            vars[hash[0]] = hash[1];
-        }
-        return vars;
-    },
-
-    urlParam: function(name) {
-        return $.urlParams()[name];
-    },
-
-    ajaxyClick: function($body) {
-        return function(event) {
-            event.preventDefault();
-            $.ajaxyLoad($body, this.href);
-        }
-    },
-
-    ajaxyLoad: function($body, href) {
-        var $self = $body;
-        $.get(href, {}, function(response) {
-            $self.html(response);
-            $self.css('backgroundColor', 'yellow');
-            $self.animate({backgroundColor: 'white'}, 2000);
-            $self.trigger('content-ready');
-        });
-    },
-
-    ajaxifyPanel: function($main_panel, $form_success_action) {
-        $main_panel.find('a').unbind('click.tabsShow').bind('click.tabsShow', $.ajaxyClick($main_panel));
-        $main_panel.find('form').ajaxForm({
-            target: $main_panel,
-            error: function(xhr) {
-                $main_panel.html(xhr.responseText);
-                $main_panel.trigger('content-ready');
-            },
-            success: $form_success_action
-        });
-    },
-
-    tabsShow: function() {
-        return function(event, ui) {
-            var $panel = $(ui.panel);
-            var $selected_tab = ui.index;
-            var $container_panel = $panel.find('div.container');
-            var $main_panel = $('.' + $container_panel.attr('main-content'));
-            var $side_panel = $('.' + $container_panel.attr('side-content'));
-
-            $side_panel.find('a').unbind('click.tabsShow').bind('click.tabsShow', $.ajaxyClick($main_panel));
-            var $form_success_action = $.formSuccessAction($selected_tab);
-
-            $main_panel.unbind('content-ready.tabsShow').bind('content-ready.tabsShow', function(event) {
-                $.ajaxifyPanel($(this), $form_success_action);
-            });
-
-            $.ajaxifyPanel($main_panel, $form_success_action);
-        }
-    },
-
-    formSuccessAction: function(selected_tab) {
-        return function(responseText, statusText, xhr) {
-            var new_resource_path = xhr.getResponseHeader("Location").replace(/https?:\/\/.*?\//, '/');
-            location.href = location.pathname + '?selected=' + new_resource_path + '&tab=' + selected_tab;
-        }
-    }
-});
-
-
 $(function() {
     // focus
     $('.focus').first().focus();
+    // submit on change
+    $('.submit_on_change').submitOnChange();
+    // auto hide flash messages
+    $('.flash_info, .flash_error').collapse();
     // treeify
     $('div.treeview ul').treeview();
     // tabify
@@ -88,44 +19,7 @@ $(function() {
         show: $.tabsShow(),
         selected: $.urlParam('tab')
     });
-});
-
-/*
- auto hide flash messages
- */
-
-$(function() {
-    $('.flash_info, .flash_error').delay(3000).animate({height: 0}, 1000, function() {
-        $(this).hide();
-    });
-});
-
-
-// submit on change
-$(function() {
-    $('.submit_on_change').bind('change', function() {
-        $(this).parent().submit();
-    });
-});
-
-// ajaxify 2_col_content
-$(function() {
-    var $container_panels = $('.2_col_panel');
-    $container_panels.each(function(index, element) {
-        var $container_panel = $(element);
-        var $side_panel = $('.' + $container_panel.attr('side-content'));
-        var $main_panel = $('.' + $container_panel.attr('main-content'));
-
-        var $result = $side_panel.find('a').first();
-        console.log($.urlParam('selected'))
-        if ($.urlParam('selected') != undefined) {
-            var selected = $.urlParam('selected');
-            var $required_elem = $side_panel.find("a[href$='" + selected + "']");
-            if ($required_elem.size() != 0)
-                $result = $required_elem;
-        }
-        $.ajaxyLoad($main_panel, $result.attr('href'));
-    });
+    $('.2_col_panel').prepare2PanelLayout();
 });
 
 
---
+++ public/javascripts/js/jquery.application.js
@@ -0,0 +1,148 @@
+;
+(function($) {
+    $.extend({
+        urlParams: function() {
+            var vars = [], hash;
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).replace(/#.*$/gi, '').split('&');
+            for (var i = 0; i < hashes.length; i++) {
+                hash = hashes[i].split('=');
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+            return vars;
+        },
+
+        urlParam: function(name) {
+            return $.urlParams()[name];
+        },
+
+        tabsShow: function() {
+            return function(event, ui) {
+                var $panel = $(ui.panel);
+                var $selected_tab = ui.index;
+                var $container_panel = $panel.container();
+                var $main_panel = $container_panel.mainContent();
+                var $side_panel = $container_panel.sideContent();
+
+                $side_panel.ajaxifyLinks($main_panel);
+                var $form_success_action = $.formSuccessAction($selected_tab);
+
+                $main_panel.safeBind('content-ready', function(event) {
+                    $(this).ajaxifyPanel($form_success_action);
+                });
+
+                $main_panel.ajaxifyPanel($form_success_action);
+            }
+        },
+
+        formSuccessAction: function(selected_tab) {
+            return function(responseText, statusText, xhr) {
+                var new_resource_path = xhr.getResponseHeader("Location").replace(/https?:\/\/.*?\//, '/');
+                location.href = location.pathname + '?selected=' + new_resource_path + '&tab=' + selected_tab;
+            }
+        }
+    });
+
+
+    $.fn.extend({
+        ajaxifyLinks: function($out_panel) {
+            return this.each(function() {
+                $(this).find('a').safeBind('click', $out_panel.ajaxyClick());
+            });
+        },
+
+        ajaxyClick: function() {
+            var $self = $(this);
+            return function(event) {
+                event.preventDefault();
+                $self.ajaxyLoad(this.href);
+            }
+        },
+
+        ajaxyLoad: function(href) {
+            return this.each(function() {
+                var $self = $(this);
+                $.get(href, {}, function(response) {
+                    $self.html(response);
+                    $self.css('backgroundColor', 'yellow');
+                    $self.animate({backgroundColor: 'white'}, 2000);
+                    $self.trigger('content-ready');
+                });
+            });
+        },
+
+        collapse: function(options) {
+            var settings = {
+                delay: 3000,
+                animate: {height: 0},
+                time_period: 1000
+            };
+            if (options) $.extend(options, settings);
+
+            return this.each(function() {
+                $(this).delay(settings.delay).animate(settings.animate, settings.time_period, function() {
+                    $(this).hide();
+                });
+            });
+        },
+
+        submitOnChange: function() {
+            return this.each(function() {
+                $(this).safeBind('change', function() {
+                    $(this).parent().submit();
+                });
+            });
+        },
+
+        ajaxifyPanel: function($form_success_action) {
+            return this.each(function() {
+                var $self = $(this);
+                $self.find('a').safeBind('click', $self.ajaxyClick());
+                $self.find('form').ajaxForm({
+                    target: $self,
+                    error: function(xhr) {
+                        $self.html(xhr.responseText);
+                        $self.trigger('content-ready');
+                    },
+                    success: $form_success_action
+                });
+            });
+        },
+
+        safeBind: function(event, handler) {
+            return this.each(function() {
+                $(this).unbind(event + '.application').bind(event + '.application', handler);
+            });
+        },
+
+        container: function() {
+            return this.find('div.container');
+        },
+
+        sideContent: function() {
+            return this.find('.' + this.attr('side-content'));
+        },
+
+        mainContent: function() {
+            return this.find('.' + this.attr('main-content'));
+        },
+
+        prepare2PanelLayout: function() {
+            return this.each(function(index, element) {
+                var $container_panel = $(element);
+                var $side_panel = $container_panel.sideContent();
+                var $main_panel = $container_panel.mainContent();
+
+                var $result = $side_panel.find('a').first();
+                if ($.urlParam('selected') != undefined) {
+                    var selected = $.urlParam('selected');
+                    var $required_elem = $side_panel.find("a[href$='" + selected + "']");
+                    if ($required_elem.size() != 0)
+                        $result = $required_elem;
+                }
+                $main_panel.ajaxyLoad($result.attr('href'));
+            });
+        }
+    });
+})(jQuery);
+
